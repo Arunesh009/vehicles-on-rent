04abe49f2b3a803a513e61737d03b9ab99f86967
ARUNESH SONI <140083831+Arunesh009@users.noreply.github.com>
2024-09-02 00:04:00 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<li class="about">
+	<p>We are providing online plateform that rents cars and other vehicles to clients at lower costs.</p>
+	<ul>
+		<li><a href="https://www.facebook.com/arunesh.soni.12" class="facebook" target="_blank"></a></li>
+		<li><a href="#" class="twitter" target="_blank"></a></li>
+	</ul>
+</li>
